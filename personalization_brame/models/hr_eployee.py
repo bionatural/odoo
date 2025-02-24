ba8be4b93651d7d547a94f1c974a04b209c9cc7f
bionatural <bionatural.odoo@gmail.com>
2025-02-24 13:55:48 -0600

    Add files via upload

---
+++ personalization_brame/models/hr_eployee.py
@@ -0,0 +1,15 @@
+import re
+from odoo import models, api
+from odoo.exceptions import ValidationError
+
+# /^([A-Z][AEIOUX][A-Z]{2}\d{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[12]\d|3[01])[HM](?:AS|B[CS]|C[CLMSH]|D[FG]|G[TR]|HG|JC|M[CNS]|N[ETL]|OC|PL|Q[TR]|S[PLR]|T[CSL]|VZ|YN|ZS)[B-DF-HJ-NP-TV-Z]{3}[A-Z\d])(\d)$/
+
+class HrEmployee(models.Model):
+    _inherit = "hr.employee"  # 🔹 Heredamos el modelo de empleados
+
+    @api.constrains("l10n_mx_curp")
+    def _check_curp(self):
+        curp_regex = r"^[A-Z]{4}\d{6}[HM]{1}[A-Z]{5}[0-9A-Z]{2}$"
+        for record in self:
+            if record.l10n_mx_curp and not re.match(curp_regex, record.l10n_mx_curp):
+                raise ValidationError("La CURP ingresada no es válida. Verifica su formato.")
